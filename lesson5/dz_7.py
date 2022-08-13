c69e5f39086215c99c577cf0331dd101dbed6d81
NaceXD <zxnacexz@bk.ru>
2022-08-13 18:09:23 +0300

    Домашнее задание

--- lesson5/dz_7.py
+++ lesson5/dz_7.py
@@ -1,17 +1,17 @@
 import json
 
-data = [{}, {}]
-data[1]['average_profit'] = 0
+stat = [{}, {}]
+stat[1]['average_profit'] = 0
 with open('dz7.txt', 'r', encoding='utf-8') as file:
     file.readline()
-    num = 0
+    numb = 0
     for line in file:
         print(line.split())
-        num += 1
+        numb += 1
         name = line.split()[0]
         profit = float(line.split()[2]) - float(line.split()[3])
-        data[0][name] = profit
-        data[1]['average_profit'] += profit
-    data[1]['average_profit'] /= profit
+        stat[0][name] = profit
+        stat[1]['average_profit'] += profit
+    stat[1]['average_profit'] /= profit
 with open('dz7.json', 'w', encoding='utf-8') as j_file:
-    json.dump(data, j_file)
+    json.dump(stat, j_file)
